1b4b27111a4d786bc56cfd3b1b83f2e3d86fafff
Christophe Cloquet <ccloquet@users.noreply.github.com>
2016-10-31 19:26:13 +0100

    Add files via upload

--- streets_list.py
+++ streets_list.py
@@ -20,7 +20,6 @@ import  configparser
 Config = configparser.ConfigParser()
 Config.read("config.poppy")
 
-
 x0_real         = int(Config.get('config', 'x0_real'))
 y0_real         = int(Config.get('config', 'y0_real'))
 x1_real         = int(Config.get('config', 'x1_real'))
@@ -32,8 +31,8 @@ ny              = int(Config.get('config', 'ny'))
 a               = Config.get('config', 'a').split(',')
 b               = Config.get('config', 'b').split(',')
 
-x1_delta = x0_real + nx * delta1
-y1_delta = y0_real + ny * delta1
+x1_delta        = x0_real + nx * delta1
+y1_delta        = y0_real + ny * delta1
 
 # 1. gets the list of the municipalities in the rectangle
 # -------------------------------------------------------
@@ -79,10 +78,10 @@ for name in names:
         yMin = street['yMin'];  yMax = street['yMax']
 
         eta = (xMin-x0_real)/delta1; gridX0 = math.floor(eta); sX0 = math.floor((eta-math.floor(eta))*2);
-        eta = (y0_real-yMin)/delta1; gridY0 = math.floor(eta); sY0 = math.floor((eta-math.floor(eta))*2);
+        eta = (y0_real-yMin)/delta1; gridY0 = 1+math.floor(eta); sY0 = math.floor((eta-math.floor(eta))*2);
     
         eta = (xMax-x0_real)/delta1; gridX1 = math.floor(eta); sX1 = math.floor((eta-math.floor(eta))*2);
-        eta = (y0_real-yMax)/delta1; gridY1 = math.floor(eta); sY1 = math.floor((eta-math.floor(eta))*2);
+        eta = (y0_real-yMax)/delta1; gridY1 = 1+math.floor(eta); sY1 = math.floor((eta-math.floor(eta))*2);
 
         # streets that are out of the rectangle even though if a part of the municipality is in the rectangle
         if gridX0 >= len(a):
@@ -114,15 +113,15 @@ for name in names:
             u+= ' - ' + v
 
         # extract the relevant part of the name, to sort
-        na = street['nom'].title().replace(' De ', ' de ').replace(' Du ',' du ').replace(' Des ', ' des ').replace(' La ', ' la ')
+        na = street['nom'].title().replace(' De ', ' de ').replace(" D' ", " d'").replace(' Du ',' du ').replace(' Des ', ' des ').replace(' La ', ' la ').replace(' Aux ', ' aux ')
         nb = na
-        for pre in ['Allée', 'Chemin', 'Rue', 'Avenue', 'Chaussée', 'Boulevard', 'Route', 'Ruelle', 'Place', 'Sentier', 'Voie', 'Thier', 'Pré', 'Plateau', 'Fond', 'Clos', 'Tige', 'Quai', 'Porte', 'Bois', 'Impasse', 'Drève', 'Fond', 'Voisinage', 'Venelle']:
-            nb = nb.replace(pre + ' de la ', '')
-            nb = nb.replace(pre + ' de l\' ', '')
-            nb = nb.replace(pre + ' de ', '').replace(pre + ' des ', '').replace(pre + ' du ', '').replace(pre + ' aux ', '')
+        for pre in ['Allée', 'Chemin', 'Rue', 'Avenue', 'Chaussée', 'Carrefour', 'Cour', 'Cité', 'Camp', 'Espace', 'Tienne', 'Tour', 'Boulevard', 'Route', 'Ruelle', 'Place', 'Sentier', 'Voie', 'Thier', 'Pré', 'Plateau', 'Fond', 'Clos', 'Tige', 'Quai', 'Porte', 'Bois', 'Impasse', 'Drève', 'Fond', 'Voisinage', 'Venelle']:
+            nb = nb.replace(pre + ' de la ', '')'Square', 
+            nb = nb.replace(pre + " de l'", '')
+            nb = nb.replace(pre + ' de ', '').replace(pre + ' des ', '').replace(pre + ' aux ', '').replace(pre + ' du ', '').replace(pre + ' aux ', '').replace(pre + " d' ", '')
             nb = nb.replace(pre + ' ', '')
             
-        tab.append([na, nb, u])       # array by municipality
+        tab.append([na, nb, u])             # array by municipality
         gentab.append([name, na, nb, u])    # global array
 
     if len(tab) > 0:
@@ -131,6 +130,6 @@ for name in names:
         print('-------------------------')
         print(tabulate.tabulate(tab))
 
-with open("streets.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(gentab)
+#with open("streets.csv", "w") as f:
+#    writer = csv.writer(f)
+#    writer.writerows(gentab)
